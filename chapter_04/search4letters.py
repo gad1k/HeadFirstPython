9a77ae6c0d139e325b69c90645ee08904233ed9c
Andrei Hryshechkin <gad1k@list.ru>
2021-01-08 15:09:45 +0300

    Added first module

--- chapter_04/search4letters.py
+++ chapter_04/search4letters.py
@@ -1,9 +1,9 @@
-def search4vowels(phrase: str) -> set:
-    """Возвращает гласные, найденные в указанной фразе"""
-    vowels = set("aeiou")
-    return vowels.intersection(set(phrase))
-
-
 def search4letters(phrase: str, letters: str = "aeiou") -> set:
     """Возвращает множество букв из "letters", найденных в указанной фразе"""
     return set(letters).intersection(set(phrase))
+
+
+help(search4letters)
+print(search4letters("Python", "ot"))
+print(search4letters("Python"
+                     ))
