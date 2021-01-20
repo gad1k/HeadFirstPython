729f1284b7dda045ff0ab54239465dd3e3159f21
Andrei Hryshechkin <gad1k@list.ru>
2021-01-20 21:11:51 +0300

    Added hello_flask.py file and changed module files

--- chapter_04/vsearch/setup.py
+++ chapter_04/vsearch/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="search_tools",
+    name="vsearch",
     version="1.0",
     description="The Head First Python Search Tools",
     author="HF Python 2e",
