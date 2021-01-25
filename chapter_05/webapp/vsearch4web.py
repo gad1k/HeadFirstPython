89d82a3c1a57851f5e63318c7ef3b4c3bc8da0e4
Andrei Hryshechkin <gad1k@list.ru>
2021-01-25 21:19:02 +0300

    Added new feature in the webapp - viewlog

--- chapter_05/webapp/vsearch4web.py
+++ chapter_05/webapp/vsearch4web.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+
+def log_request(req: "flask_request", res: str) -> None:
+    with open("vsearch.log", "a") as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep="|")
 
 
 @app.route("/search4", methods=["POST"])
@@ -10,6 +15,7 @@ def do_search() -> "html":
     letters = request.form["letters"]
     title = "Here are your results:"
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
 
     return render_template("results.html", the_phrase=phrase, the_letters=letters, the_title=title, the_results=results)
 
@@ -18,6 +24,18 @@ def do_search() -> "html":
 @app.route("/entry")
 def entry_page() -> "html":
     return render_template("entry.html", the_title="Welcome to search4letters on the web!")
+
+
+@app.route("/viewlog")
+def view_the_log() -> "html":
+    contents = []
+    with open("vsearch.log") as log:
+        for line in log:
+            contents.append([])
+            for item in line.split("|"):
+                contents[-1].append(escape(item))
+    titles = ("Form Data", "Remote Address", "User Agent", "Results")
+    return render_template("viewlog.html", the_title="View Log", the_row_titles=titles, the_data=contents)
 
 
 if __name__ == "__main__":
