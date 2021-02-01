1b9978adc2bed1437f2a775ae70ece47adcf492e
Andrei Hryshechkin <gad1k@list.ru>
2021-02-01 23:47:27 +0300

    Showing the work of decorator

--- chapter_10/simple_webapp.py
+++ chapter_10/simple_webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -9,16 +10,19 @@ def hello() -> str:
 
 
 @app.route("/page1")
+@check_logged_in
 def page1() -> str:
     return "This is page 1"
 
 
 @app.route("/page2")
+@check_logged_in
 def page2() -> str:
     return "This is page 2"
 
 
 @app.route("/page3")
+@check_logged_in
 def page3() -> str:
     return "This is page 3"
 
@@ -33,13 +37,6 @@ def do_login() -> str:
 def do_logout() -> str:
     session.pop("logged_in")
     return "You are now logged out"
-
-
-@app.route("/status")
-def check_status() -> str:
-    if "logged_in" in session:
-        return "You are currently logged in"
-    return "You are NOT logged in"
 
 
 app.secret_key = "YouWillNeverGuess"
