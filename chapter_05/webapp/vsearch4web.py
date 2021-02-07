b8740a57be57b927d9bcfcb404eef5e04f5d4e0c
Andrei Hryshechkin <gad1k@list.ru>
2021-02-07 23:08:51 +0300

    Added new feature in the webapp - exception handling

--- chapter_05/webapp/vsearch4web.py
+++ chapter_05/webapp/vsearch4web.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, session
+from flask import copy_current_request_context
+
 from vsearch import search4letters
 
-from dbcm import UseDatabase
+from dbcm import UseDatabase, ConnectionsError, CredentialsError, SQLError
 from checker import check_logged_in
+
+from time import sleep
 
 app = Flask(__name__)
 
@@ -27,24 +31,29 @@ def do_logout() -> str:
     return "You are now logged out"
 
 
-def log_request(req: "flask_request", res: str) -> None:
-    """Журналирует веб-запрос и возвращает результаты"""
-    with UseDatabase(app.config["dbconfig"]) as cursor:
-        sql = """
-              insert into log (phrase, letters, ip, browser_string, results)
-              values (%s, %s, %s, %s, %s)
-              """
-        cursor.execute(sql, (req.form["phrase"], req.form["letters"], req.remote_addr, req.user_agent.browser, res))
-
-
 @app.route("/search4", methods=["POST"])
 def do_search() -> "html":
     """Извлекает данные из запроса, выполняет поиск, возвращает результаты"""
+
+    @copy_current_request_context
+    def log_request(req: "flask_request", res: str) -> None:
+        """Журналирует веб-запрос и возвращает результаты"""
+        sleep(15)
+        with UseDatabase(app.config["dbconfig"]) as cursor:
+            sql = """
+                  insert into log (phrase, letters, ip, browser_string, results)
+                  values (%s, %s, %s, %s, %s)
+                  """
+            cursor.execute(sql, (req.form["phrase"], req.form["letters"], req.remote_addr, req.user_agent.browser, res))
+
     phrase = request.form["phrase"]
     letters = request.form["letters"]
     title = "Here are your results:"
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print("Logging failed with this error:", str(err))
 
     return render_template("results.html", the_phrase=phrase, the_letters=letters, the_title=title, the_results=results)
 
@@ -60,16 +69,26 @@ def entry_page() -> "html":
 @check_logged_in
 def view_the_log() -> "html":
     """Выводит данные из БД в HTML-таблицы"""
-    with UseDatabase(app.config["dbconfig"]) as cursor:
-        sql = """
-              select phrase, letters, ip, browser_string, results
-                from log
-              """
-        cursor.execute(sql)
-        contents = cursor.fetchall()
+    try:
+        with UseDatabase(app.config["dbconfig"]) as cursor:
+            sql = """
+                  select phrase, letters, ip, browser_string, results
+                    from log
+                  """
+            cursor.execute(sql)
+            contents = cursor.fetchall()
 
-    titles = ("Phrase", "Letters", "Remote Address", "User Agent", "Results")
-    return render_template("viewlog.html", the_title="View Log", the_row_titles=titles, the_data=contents)
+        titles = ("Phrase", "Letters", "Remote Address", "User Agent", "Results")
+        return render_template("viewlog.html", the_title="View Log", the_row_titles=titles, the_data=contents)
+    except ConnectionsError as err:
+        print("Is your database switched on? Error:", str(err))
+    except CredentialsError as err:
+        print("User ID/Password issues. Error:", str(err))
+    except SQLError as err:
+        print("Is your query correct? Error:", str(err))
+    except Exception as err:
+        print("Something went wrong:", str(err))
+    return "Error"
 
 
 app.secret_key = "YouWillNeverGuessMySecretKey"
