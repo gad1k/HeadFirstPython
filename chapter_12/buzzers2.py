3cd0e3de3c447286e0b6d0cbf0592879a95d8de8
Andrei Hryshechkin <gad1k@list.ru>
2021-02-18 14:55:23 +0300

    Minor cosmetic changes

--- chapter_12/buzzers2.py
+++ chapter_12/buzzers2.py
@@ -14,4 +14,3 @@ print(flights)
 print()
 
 pprint(flights)
-
--- chapter_12/url_utils2.py
+++ chapter_12/url_utils2.py
@@ -4,4 +4,3 @@ urls = ("http://github.com", "http://oreilly.com", "http://yandex.by")
 
 for resp in (requests.get(url) for url in urls):
     print(len(resp.content), "->", resp.status_code, "->", resp.url)
-
